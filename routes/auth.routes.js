0e02fd1bc8ccd9b3222263bf7a3048e3d52e5edb
Panthari Panthong <panthari.panthong@gmail.com>
2023-05-18 16:39:01 +0200

    middleware setup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,8 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User.model");
 
+const { isAuthenticated } = require("./../middleware/jwt.middleware");
+
 const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
@@ -87,14 +89,24 @@ router.post("/login", async (req, res, next) => {
         }
       );
 
-      res.status(200).json({ message: "User login" });
+      res.status(200).json({ authToken: authToken });
     } else {
       res.status(401).json({ message: "Unable to authenticate the user" });
     }
   } catch (error) {
-    console.log(error);
-    // res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
+});
+
+// GET  /auth/verify  -  Used to verify JWT stored on the client
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  // console.log(`req.payload`, req.payload);
+
+  // Send back the object with user data
+  // previously set as the token payload
+  res.status(200).json(req.payload);
 });
 
 module.exports = router;
