04d541e09a207920ea966db8e7784caef5b8a084
solenwa <solen.wanono@gmail.com>
2023-05-22 13:01:40 +0200

    Post route for comments OK

--- routes/place.routes.js
+++ routes/place.routes.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const Place = require("../models/Place.Model");
+const Post = require("../models/Post.Model");
 
 router.get("/places", async (req, res, next) => {
   try {
-    const response = await Place.find();
-    res.send({ data: response });
+    const responsePlace = await Place.find();
+    res.send({ data: responsePlace });
+    const responsePost = await Post.find();
+    res.send({ data: responsePost });
   } catch (error) {
     console.log(error);
   }
@@ -14,6 +17,14 @@ router.get("/places/:id", (req, res, next) => {
   Place.findById(req.params.id)
     .then((place) => res.status(200).json(place))
     .catch((error) => res.json(error));
+});
+
+router.post("/places/:id", (req, res, next) => {
+  const { comment } = req.body;
+
+  Post.create({ comment, place: req.params.id }) // + user to add if logged in
+    .then((response) => res.json(response))
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
