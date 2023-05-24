991e4fdfbf29601e2e977402915c8ddd26845168
solenwa <solen.wanono@gmail.com>
2023-05-24 16:13:05 +0200

    Delete function added for posts

--- routes/place.routes.js
+++ routes/place.routes.js
@@ -85,6 +85,7 @@ router.patch("/mymap/addtoVisit/:userId", async (req, res) => {
   }
 });
 
+//  Route to get one place's details
 router.get("/places/:id", (req, res, next) => {
   Place.findById(req.params.id)
     .populate("post")
@@ -92,16 +93,17 @@ router.get("/places/:id", (req, res, next) => {
     .catch((error) => res.json(error));
 });
 
+// Route to create a post on a place's detail page
 router.post("/places/:id", isAuthenticated, (req, res, next) => {
   const { comment } = req.body;
 
   Post.create({ user: payload, comment, place: req.params.id })
     .then((newPost) => {
-      return Place.findByIdAndUpdate(req.params.id, {
+      Place.findByIdAndUpdate(req.params.id, {
         $push: { post: newPost._id },
       });
     })
-    .then((response) => res.json(response))
+    .then((response) => res.status(200).json(response))
     .catch((err) => res.json(err));
 });
 
